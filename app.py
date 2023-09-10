d31390e4a1cd4d783cb3f0bf6be387a306e71d98
Rodrigo Pereira Faria <rodrigopereirafaria9@gmail.com>
2023-09-10 17:17:33 -0300

    Alterações em código Python

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-x = input("Digite seu nome: ")
-print(f" Seu nome é: {x}")
+nome = input("Digite seu nome: ")
+print(f" Seu nome é: {nome}")
+numchar = len(nome)
+print(f"Seu nome possui {numchar} caracteres")
+
+if(numchar > 8):
+    print("Seu nome é Grande.")
+else:
+    print("Seu nome é pequeno.")
--- vers.py
+++ vers.py
@@ -1,2 +1,11 @@
-x = input("Digite uma idade:")
-input(f"Sua idade é {x}")
+idade1 = int(input("Digite uma idade:"))
+print(f"Primeira idade é {idade1} anos")
+
+idade2 = int(input("Digite outra idade:"))
+print(f"Segunda idade é {idade2}")
+
+if(idade1>idade2):
+    diferença = idade1 - idade2
+else:
+    diferença = idade2 - idade1
+print(f"A diferença entre as idades é {diferença} anos")
